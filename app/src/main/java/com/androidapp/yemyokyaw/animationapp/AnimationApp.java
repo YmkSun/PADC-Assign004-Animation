039cf666c363b44117995375291b0c4c77f14178
Ye Myo Kyaw <ymksun90@gmail.com>
2017-12-01 00:48:54 +0630

    Skeleton Structure

---
+++ app/src/main/java/com/androidapp/yemyokyaw/animationapp/AnimationApp.java
@@ -0,0 +1,10 @@
+package com.androidapp.yemyokyaw.animationapp;
+
+import android.app.Application;
+
+/**
+ * Created by yemyokyaw on 12/1/17.
+ */
+
+public class AnimationApp extends Application {
+}
